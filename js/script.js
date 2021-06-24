41423839e651c162d4788f3c7eaabc0244a26e55
George Hlongwane <55334979+V-FOR-VEND3TTA@users.noreply.github.com>
2021-06-25 01:46:04 +0200

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function openModal() {
+  document.getElementById("myModal").style.display = "inherit";
+  document.body.style.overflow = "hidden";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
